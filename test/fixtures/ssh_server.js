749e0c446dca3b6571815e8e2aed1829e698287b
vmarchaud <contact@vmarchaud.fr>
2016-12-10 12:53:32 +0100

    (tests) create ssh server fixtures

---
+++ test/fixtures/ssh_server.js
@@ -0,0 +1,52 @@
+
+'use scrict';
+
+const fs = require('fs');
+const crypto = require('crypto');
+const inspect = require('util').inspect;
+const ssh2 = require('ssh2');
+const utils = ssh2.utils;
+const EventEmitter = require('events');
+
+class EventBus extends EventEmitter {}
+
+export class ServerWrapper {
+
+  constructor () {}
+
+  static function buildServer(opts) {
+    if (opts.privateKey && !opts.publicKey)
+     opts.publicKey = utils.genPublicKey(opts.privateKey);
+    const eventBus = new EventBus();
+    const server = new ssh2.Server({
+      hostKeys: [ this.opts.privateKey ]
+    }, function(client) {
+      client.on('authentication', (ctx) => {
+        if (this.methods.indexOf(ctx.method) < 0)
+          return ctx.reject();
+        // auth using password
+        if (ctx.method === 'password' 
+            && ctx.username === opts.userrname
+            && ctx.password === opts.password)
+          return ctx.accept();
+        // auth using public key
+        if (ctx.method === 'publickey' && ctx.key.algo === opts.publicKey.fulltype
+                && ctx.key.data.equals(publicKey.public)) {
+          if (!ctx.signature) return ctx.accept();
+
+          var verifier = crypto.createVerify(ctx.sigAlgo);
+          verifier.update(ctx.blob);
+          return verifier.verify(pubKey.publicOrig, ctx.signature) ? 
+              ctx.accept() : ctx.reject();
+        }
+      }).on('ready', () => {
+        client.on('session', (accept, reject) => {
+          eventBus.emit('session', accept());
+        });
+      }).on('end', () => {
+        client.emit('disconnect')
+      });
+    }).listen(opts.port, opts.host, () => {});
+    return { bus: eventBus, server: server };
+  }
+}
